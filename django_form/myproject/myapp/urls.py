30dc6fb9afd47bfc49029823b15576ea13eb0af8
Navneeth Krishna <navneeth242003@gmail.com>
2024-11-07 11:55:09 +0530

    added admin

--- django_form/myproject/myapp/urls.py
+++ django_form/myproject/myapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
+
+admin.site.site_title = "Your info"
+admin.site.site_header = "Info page"
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('submit/', views.submit_form, name='submit_form'),
     path('thank-you/', views.thank_you, name='thank_you'),
     path('entries/', views.view_entries, name='view_entries'),
